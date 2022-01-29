c91950ce9a79ac47d6a899dc245534d4595d930b
Camilo Muñoz <ancamu99@hotmail.com>
2022-01-29 18:00:45 -0500

    Pequeños cambios
    Agregados botones de la tabla

--- angular-directory/src/app/components/menu-card/menu-card.component.ts
+++ angular-directory/src/app/components/menu-card/menu-card.component.ts
@@ -1,43 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, Input } from "@angular/core";
+import { Router } from "@angular/router";
 //Entities
-import { Contact } from '../../entities/contact';
+import { Contact } from "../../entities/contact";
 
 @Component({
-  selector: 'app-menu-card',
-  templateUrl: './menu-card.component.html',
-  styleUrls: ['./menu-card.component.css']
+  selector: "app-menu-card",
+  templateUrl: "./menu-card.component.html",
+  styleUrls: ["./menu-card.component.css"],
 })
 export class MenuCardComponent implements OnInit {
   @Input() contact: Contact;
-  modal:boolean=false;
-  
-  //table info for phone numbers. 
-  titlesPhone: String[] = ['country','label','number']; //titles of the table
-  attributesPhone: any[] = [['country'],['label'],['number']];
+  modal: boolean = false;
+
+  //table info for phone numbers.
+  titlesPhone: String[] = ["country", "label", "number", "delete", "edit"]; //titles of the table
+  attributesPhone: any[] = [["country"], ["label"], ["number"]];
 
   //table info for addresses
-  titlesAddresses: String[] = ['label','address']; //titles of the table
-  attributesAddresses: any[] = [['label'],['address']];
-  
-  constructor(
-    public router: Router
-  ){ }
-  
-  ngOnInit(){
+  titlesAddresses: String[] = ["label", "address", "delete", "edit"]; //titles of the table
+  attributesAddresses: any[] = [["label"], ["address"]];
 
-  }
+  constructor(public router: Router) {}
+
+  ngOnInit() {}
 
   /**
-   * Show contact information on modal 
+   * Show contact information on modal
    */
-  showModal(){
+  showModal() {
     this.modal = true;
   }
   /**
-   * Close contact model 
+   * Close contact model
    */
-  closeModal(){
+  closeModal() {
     this.modal = false;
   }
 }
